c74be00dcfb66bda285ea5a3e4e797c3ee3d1463
Buhbah6 <thegoldenrailway@gmail.com>
2022-02-22 11:32:22 -0500

    Started profile stuff

--- Assignment 2/app/controllers/Profile.php
+++ Assignment 2/app/controllers/Profile.php
@@ -8,10 +8,31 @@
             }
 
             public function create() {
-                $this->view('Profile/create');
+                if(!isset($_POST['action'])) {
+                    $this->view('Profile/create');
+                }
+                else {
+                    $userProfile = new \app\models\Profile();
+                    $userProfile->first_name=$_POST['first_name'];
+                    $userProfile->middle_name=$_POST['middle_name'];
+                    $userProfile->last_name=$_POST['last_name'];
+                    $userProfile->insert();
+                    echo "Profile Created";
+                }
             }
 
-            public function update() {
-                $this->view('Profile/update');
+            public function update($profile_id) {
+                $profile = new \app\models\Profile();
+                $profile = $profile->get($profile_id);
+                if(!isset($_POST['action'])){
+                    $this->view('Profile/update', $profile);
+                }
+                else {
+                    $userProfile->first_name=$_POST['first_name'];
+                    $userProfile->middle_name=$_POST['middle_name'];
+                    $userProfile->last_name=$_POST['last_name'];
+                    $profile->update();
+                    header('location:/Profile/index');
+                }
             }
         }
--- Assignment 2/app/models/Profile.php
+++ Assignment 2/app/models/Profile.php
@@ -1,6 +1,43 @@
 <?php
     namespace app\models;
 
-        class Profile extends \app\core\Model {
-            
+    class Profile extends \app\core\Model {
+        
+        function __construct(){
+            parent::__construct();
         }
+
+        function get($profile_id){
+            $SQL = 'SELECT * FROM profile WHERE profile_id = :profile_id';
+            $STMT = self::$_connection->prepare($SQL);
+            $STMT->execute(['profile_id'=>$profile_id]);
+            $STMT->setFetchMode(\PDO::FETCH_CLASS, "app\models\Profile");
+            return $STMT->fetch();
+        }
+
+        function getAll() {
+            $SQL = 'SELECT * FROM profile';
+            $STMT = self::$_connection->prepare($SQL);
+            $STMT->execute();
+            $STMT->setFetchMode(\PDO::FETCH_CLASS, "app\models\Profile");
+            return $STMT->fetchAll();
+        }
+
+        function insert(){
+            $SQL = 'INSERT INTO profile(first_name, middle_name, last_name) VALUES(:first_name, :middle_name, :last_name)';
+            $STMT = self::$_connection->prepare($SQL);
+            $STMT->execute(['first_name'=>$this->first_name, 'middle_name'=>$this->middle_name, 'last_name'=>$this->last_name]);
+        }
+
+        function update(){
+            $SQL = 'UPDATE profile SET first_name = :first_name, middle_name = :middle_name, last_name = :last_name WHERE profile_id = :profile_id';
+            $STMT = self::$_connection->prepare($SQL);
+            $STMT->execute(['first_name'=>$this->first_name, 'middle_name'=>$this->middle_name, 'last_name'=>$this->last_name, 'profile_id'=>$this->profile_id]);
+        }
+
+        function delete($profile_id){
+            $SQL = 'DELETE FROM profile WHERE profile_id = :profile_id';
+            $STMT = self::$_connection->prepare($SQL);
+            $STMT->execute(['profile_id'=>$profile_id]);
+        }
+    }
